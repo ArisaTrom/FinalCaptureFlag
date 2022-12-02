b17cf422a6f17523566c4d60756f6c014403e2ef
Jules <slash@chapman.edu>
2022-12-01 21:48:04 -0800

    we can now move which is cool

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    
+    // add clamping so we don't escape the map
+    
+    private GameController _gameController;
     // Start is called before the first frame update
     void Start()
     {
+        _gameController = GetComponentInParent<GameController>();
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+
+        transform.position += movement * speed;
     }
 }
